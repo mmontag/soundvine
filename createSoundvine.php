5ff55633865f564e0664ea1df51146bd9ad4fdbe
Matt Montag <montag@spotify.com>
2013-02-17 23:06:13 -0800

    Support for optional alias

--- createSoundvine.php
+++ createSoundvine.php
@@ -3,28 +3,52 @@
 require(".db.config.php");
 
 $regs = array();
-$regs['alias'] = "/^[a-zA-Z0-9\-]{2,30}$/";
+$regs['alias'] = "/^[a-zA-Z][a-zA-Z0-9\-]{1,29}$/";
 // Must be a Vine video, for now.
 $regs['video_url'] = "#^(https://vines\.s3\.amazonaws\.com/[a-zA-Z0-9_\-\./]+\.mp4\?[a-zA-Z0-9=_\.\-]+)$#";
 $regs['audio_url'] = "#^(http|https)://#";
 
 foreach ($regs as $field => $regex) {
   if (preg_match($regex, $_POST[$field]) !== 1) {
-    header(':', true, 400);
-    echo json_encode(array("error"=>"`$field` wasn't formatted correctly."));
-    exit;
+    // TODO: fix up. Getting hacky here.
+    if ($field == 'alias') {
+      unset($_POST[$field]);
+    } else {
+      header(':', true, 400);
+      echo json_encode(array("error"=>"`$field` wasn't formatted correctly."));
+      exit;
+    }
   }
 }
 
-if (isset($_POST['alias'])) {
-  $alias = $mysqli->escape_string($_POST['alias']);
-} else {
-  $alias = "NULL";
+$audio_url = $_POST['audio_url'];
+$ch = curl_init($audio_url);
+// Check headers of the audio URL and see if it's the right mime-type
+// Not completely foolproof, but good enough
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_NOBODY, 1);
+curl_exec($ch);
+$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+curl_close($ch);
+if (preg_match("/audio/", $content_type) !== 1 && preg_match("/\.(mp3|ogg|wav)$/", $audio_url) !== 1) {
+  header(':', true, 400);
+  echo json_encode(array("error"=>"No audio detected at audio URL."));
+  exit;
 }
+
 $video_url = $mysqli->escape_string($_POST['video_url']);
-$audio_url = $mysqli->escape_string($_POST['audio_url']);
+$audio_url = $mysqli->escape_string($audio_url);
 
 // Optional fields
+$_f = 'alias';
+$alias = null;
+if (isset($_POST[$_f])) {
+  // Already been validated
+  $alias = $mysqli->escape_string($_POST[$_f]);
+  $opt_fields .= "$_f, ";
+  $opt_values .= "'$alias', ";
+}
 
 $_f = 'video_speed';
 if (isset($_POST[$_f])) {
@@ -49,8 +73,8 @@ if (isset($_POST[$_f])) {
 
 $ip_address = $mysqli->escape_string($_SERVER['REMOTE_HOST']);
 
-$query = "INSERT INTO vines (alias, video_url, audio_url, $opt_fields ip_address, time_stamp)
-          VALUES ('$alias', '$video_url', '$audio_url', $opt_values '$ip_address', CURRENT_TIMESTAMP)";
+$query = "INSERT INTO vines (video_url, audio_url, $opt_fields ip_address, time_stamp)
+          VALUES ('$video_url', '$audio_url', $opt_values '$ip_address', CURRENT_TIMESTAMP)";
 $result = $mysqli->query($query);
 
 if ($mysqli->affected_rows < 1) {
@@ -62,8 +86,8 @@ if ($mysqli->affected_rows < 1) {
 $id = $mysqli->insert_id;
 echo json_encode(array(
   "success" => "Record added.",
-  "id" => $id,
   "alias" => $alias,
+  "id" => $id
 ));
 
 $mysqli->close();
